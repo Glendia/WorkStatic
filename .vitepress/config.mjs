44b2c146fabde25d76be565d5979abbb05b8e02f
Glendia <49245772+Glendia@users.noreply.github.com>
2024-07-12 22:51:31 +0800

    配置

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,23 +1,19 @@
 import { defineConfig } from 'vitepress'
-
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/',
   lang: 'zh-CN',
   title: "WorkStatic",
   description: "免费CDN内容分发托管平台",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: '首页', link: '/' },
       { text: '文档', link: '/guide/' },
       { text: '资源', link: '/sources/'},
       { text: '关于', link: '/about/'},
-      { text: '讨论', link: 'https://github.com/glendia/workstatic/iessu}
+      { text: '讨论', link: 'https://github.com/glendia/workstatic/iesssues'}
     ],
-
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/Glendia/Workstatic' }
     ]
   }
 })
