3e542bb27e6079456bce7edcc267d89fe0c8c34e
Glendia <glendia@oisx.cn>
2024-07-13 02:09:29 +0800

    修订错误

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -39,3 +39,5 @@ export default defineConfig({
       message: '备案号:<a href="https://beian.miit.gov.cn/">沪ICP备2023012559号</a>',
       copyright: 'Copyright © 2022-2024 <a href="/">WorkStatic</a> All Rights Reserved.'
     }
+    }
+    })
--- index.md
+++ index.md
@@ -35,8 +35,3 @@ features:
 
  
 <!-- 自定义组件 -->
-<script setup>
-import home from './components/home.vue';
-</script>
- 
-<home />
