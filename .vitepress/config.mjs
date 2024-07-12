f76dfc6db190a3e90d61714ed183549decdbc33a
Glendia <glendia@oisx.cn>
2024-07-12 22:40:18 +0800

    config menu

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,23 +2,18 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/',
+  lang: 'zh-CN',
   title: "WorkStatic",
   description: "免费CDN内容分发托管平台",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
-    ],
-
-    sidebar: [
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+      { text: '首页', link: '/' },
+      { text: '文档', link: '/guide/' },
+      { text: '资源', link: '/sources/'},
+      { text: '关于', link: '/about/'},
+      { text: '讨论', link: 'https://github.com/glendia/workstatic/iessu}
     ],
 
     socialLinks: [
