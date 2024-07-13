c0b52a4dc815ba9b8b1fee68ffb0a3d98d3170c3
Glendia <glendia@oisx.cn>
2024-07-13 14:30:12 +0800

    sidebar

--- .vitepress/js/sidebar.js
+++ .vitepress/js/sidebar.js
@@ -1,15 +1,13 @@
-//.vitepress/js/sidebar.js
-export const sidebar =[
+export const sidebar = {
+  '/posts/': [
     {
-      '/posts/': [
-        {
-          text: '博客',
-          items: [
-            { text: '列表', link: '/posts/' },
-            { text: '第一章', link: '/posts/pages-1' },
-            { text: '第二章', link: '/posts/pages-2'}
-          ]
-        },
-]
+      text: 'posts',
+      items: [
+        {text: 'Index',link: '/posts/',},
+        {text: 'One',link: '/posts/one',},
+        {text: 'Two',link: '/posts/two',},
+      ],
+    },
+  ],
+  
 }
-]
